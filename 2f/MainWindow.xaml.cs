f1c9f526de5fd33df86f95cd1413fa068f06ffe1
m305-11\w401 <tommy555553@gmail.com>
2018-06-14 15:23:06 +0800

    money

---
+++ 2f/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2f
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            qq q = new qq();
+
+            list.Children.Add(q);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+
+                int total = 0;
+
+
+
+                foreach (qq item in list.Children)
+                {
+
+                    total += item.money;
+                }
+
+
+                all.Text = total.ToString();
+            }
+        }
+    }
+}
---
+++ 2f/qq.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2f
+{
+    /// <summary>
+    /// qq.xaml 的互動邏輯
+    /// </summary>
+    public partial class qq : UserControl
+    {
+        public string name
+        {
+            get
+            {
+                return a.Text;
+            }
+            set
+            {
+                a.Text = value;
+            }
+        }
+        public int money
+        {
+            get
+            {
+                
+                try
+                {
+                    return int.Parse(b.Text);
+                }
+
+                catch
+                {
+                    MessageBox.Show("輸入數字");
+                    return 0;
+                }
+            }
+            set
+            {
+                b.Text = value.ToString();
+            }
+
+        }
+        public qq()
+        {
+            InitializeComponent();
+        }
+    }
+}
